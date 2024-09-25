c81a35da2d3d2a412267963e48c3aad98ad9095a
eleanorlow <eleanor.low@outlook.com>
2024-09-25 10:35:35 +1000

    testing editing

--- plottingDobot.m
+++ plottingDobot.m
@@ -6,6 +6,7 @@ function plottingDobot
     %robot.model.base = transl(linearUR3eBasePos(1,:))*trotx(linearUR3eBasePos(2,1))*troty(linearUR3eBasePos(2,2));
     robot.PlotAndColourRobot;
     pause()
-    
+    %editing the code
+        
 
 end
