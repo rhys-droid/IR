a32065866da8eaf9fceabca04436bcbbbb6fffba
eleanorlow <160460243+eleanorlow@users.noreply.github.com>
2024-09-25 10:26:06 +1000

    Add files via upload

---
+++ plottingDobot.m
@@ -0,0 +1,11 @@
+function plottingDobot
+
+     
+    robot = DobotMagician;
+    robot.CreateModel();
+    %robot.model.base = transl(linearUR3eBasePos(1,:))*trotx(linearUR3eBasePos(2,1))*troty(linearUR3eBasePos(2,2));
+    robot.PlotAndColourRobot;
+    pause()
+    
+
+end
