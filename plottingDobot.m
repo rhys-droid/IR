b53e90109627fe96e69a159522b6da9def37e865
eleanorlow <eleanor.low@outlook.com>
2024-09-27 08:38:29 +1000

    trying to correclty plot linear dobot

--- plottingDobot.m
+++ plottingDobot.m
@@ -1,12 +1,18 @@
 function plottingDobot
 
      
-    robot = DobotMagician;
+    robot = LinearDobotMagician;
     robot.CreateModel();
     %robot.model.base = transl(linearUR3eBasePos(1,:))*trotx(linearUR3eBasePos(2,1))*troty(linearUR3eBasePos(2,2));
+    
     robot.PlotAndColourRobot;
+
+    axis equal
+
+    %look at when gavin sets up the work space to then plot the robot
+
     pause()
-    %editing the code
-        
+
+ 
 
 end
