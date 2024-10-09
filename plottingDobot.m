c784bde6b862e39a88d3c426b8b82ce232237acd
eleanorlow <160460243+eleanorlow@users.noreply.github.com>
2024-10-09 13:05:26 +1100

    Add files via upload

--- plottingDobot.m
+++ plottingDobot.m
@@ -1,24 +1,21 @@
 function plottingDobot
 
-     
-    robot = LinearDobotMagician;
-    robot.CreateModel();
-    %robot.model.base = transl(linearUR3eBasePos(1,:))*trotx(linearUR3eBasePos(2,1))*troty(linearUR3eBasePos(2,2));
-    
-    % robot.PlotAndColourRobot;
+    cartPos = [0,0,0; 0,0,0];
+
+
+    robot = LinearDobot;
+    robot.PlotAndColourRobot;
+
+    hold on
+
+    hCart = PlaceObject('cart.ply', cartPos(1,:));
 
     grid on
     axis equal
 
-    axis([-4 4 -4 4 -4 4]);
-    % scale = 0.5;
-
-    % robot.plot('workspace', workspace, 'scale', scale);
-
-    robot.PlotAndColourRobot;
-
     pause()
 
  
+    
 
 end
