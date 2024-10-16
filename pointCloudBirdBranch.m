6bf693f4a3763bf91c39d27ebafc02ce409c493e
eleanorlow <eleanor.low@outlook.com>
2024-10-16 18:47:15 +1100

    trying to create 6dof

--- pointCloudBirdBranch.m
+++ pointCloudBirdBranch.m
@@ -3,7 +3,7 @@ clc
 %% Define plyFiles and robot base
 
 % birdOnBranchPos = [3,2,0;0,0,0];
-% linearDobotBase = [0,0.2,0; pi/2, 0,0]; % xyz corrdinates; xyz rotation;
+linearDobotBase = [0,0.2,0; pi/2, 0,0]; % xyz corrdinates; xyz rotation;
 threeDOfBase = [1,0,0];
 % birdOnBranch_h = PlaceObject('birdOnBranch.ply', birdOnBranchPos(1,:));
 % hold on
@@ -20,23 +20,27 @@ threeDOfBase = [1,0,0];
 % 
 % hold on
 
-%% Create 3DOF robot
+%% Create 6DOF robot
 
-L1 = Link('d',0.5,'a',0,'alpha',-pi/2,'qlim',[-2*pi 2*pi]);
-L2 = Link('d', 0, 'a', 0.612, 'alpha', 0, 'qlim', [-pi, pi]);
-L3 = Link('d', 0, 'a', 0.57, 'alpha', 0, 'qlim', [-pi, pi]);
-L4 = Link('d', 0.563, 'a', 0, 'alpha', -pi/2, 'qlim', [-pi, pi]);
-L5 = Link('d', 0.5, 'a', 0, 'alpha', pi/2, 'qlim', [-pi, pi]);
-L6 = Link('d', 0.5, 'a', 0, 'alpha', 0, 'qlim', [-pi, pi]);
+L1 = Link('d',0.3,'a',0,'alpha',-pi/2,'qlim',[-2*pi 2*pi]);
+L2 = Link('d', 0, 'a', 0.4, 'alpha', 0, 'qlim', [-pi/2, 0]);
+L3 = Link('d', 0, 'a', 0.4, 'alpha', 0, 'qlim', [0, pi/2]);
+L4 = Link('d', 0, 'a', 0, 'alpha', -pi/2, 'qlim', [-pi, pi]);
+L5 = Link('d', 0, 'a', 0, 'alpha', pi/2, 'qlim', [-pi/4, 5*pi/4]);
+L6 = Link('d', 0.2, 'a', 0, 'alpha', -pi/2, 'qlim', [-pi, pi]);
+L7 = Link('d', 0.2, 'a', 0, 'alpha', pi/2, 'qlim', [-pi, pi]);
+L8 = Link('d', 0.2, 'a', 0, 'alpha', 0, 'qlim', [-2*pi, 2*pi]);
 
-robot = SerialLink([L1, L2, L3, L4, L5, L6], 'name','myRobot');
+robot = SerialLink([L1, L2, L3, L4, L5, L6, L7, L8], 'name','myRobot');
 
-q = [0, pi/2, pi/4, 0,0,0];
+q = [0, -pi/4, pi/6, 0,0,0, 0, 0];
 
-scale = 0.1;
+scale = 0.2;
 workspace = [-0.5 1.5 -0.5 1.5 0 1];
 robot.base = transl(threeDOfBase);
-robot.teach(q, 'workspace', workspace, 'scale', scale); 
+robot.plot(q, 'workspace', workspace, 'scale', scale); 
+
+axis equal
 
 
 pause(0.1);
@@ -49,6 +53,7 @@ hold on
 % robot.model.base = transl(linearDobotBase(1,:)) * trotx(linearDobotBase(2,1)) * troty(linearDobotBase(2,2))* trotz(linearDobotBase(2,3));
 % robot.PlotAndColourRobot;
 
+%robot = TreeBot;
 
 hold off
 
