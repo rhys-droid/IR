6b4372c3497d1f8d500f63881ca17eaf2c24a053
eleanorlow <160460243+eleanorlow@users.noreply.github.com>
2024-10-24 17:03:45 +1100

    it works ;)

--- treeBotCollisionCheck.m
+++ treeBotCollisionCheck.m
@@ -2,6 +2,7 @@ classdef treeBotCollisionCheck < handle
 
     properties
         emergencyStopPressed = false;  % Class property
+        initalMove = false;
     end
 
     methods
@@ -17,43 +18,48 @@ classdef treeBotCollisionCheck < handle
         function runRobot(self)
             clf
 
-            birdOnBranchPoints = cloudPoints.loadPointClouds('birdOnBranch.ply', [1.8,0.5,0]);
-
             robot = TreeBot;
             robot.PlotAndColourRobot();
+            birdOnBranchPoints = cloudPoints.loadPointClouds('birdOnBranch.ply', [1.8,0.5,0.4]);
             
             %% Move Robot
 
-             q1 = robot.model.ikcon(transl(0.5,0.5, 0.2));
-             q2 = robot.model.ikcon(transl(1,0.3, 0));
+            if ~self.initalMove
+    
+                 q1 = robot.model.ikcon(transl(0.5,0.5, 0.2));
+                 q2 = robot.model.ikcon(transl(1,0.3, 0));
+    
+                steps = 100;
+    
+                qMatrix = jtraj(q1,q2,steps); % Obtaing the joint space trajectory
 
-            steps = 100;
-            qMatrix = jtraj(q1,q2,steps); % Obtaing the joint space trajectory
+                self.initalMove = true;
+            end
 
             self.detectES();
             
+            n = 1;
+            while n<=steps              
 
-            for n = 1:steps
+                if ~self.CheckCollision(robot.model, birdOnBranchPoints)
+                    robot.model.animate(qMatrix(n, :));
+                    n=n+1;
+                    pause(0.01)
 
-                 %currentPos = robot.fkine(robot.getpos)
-    
-                 % q1 = robot.model.ikcon(robot.model.getpos);
-                 % q2 = robot.model.ikcon(transl(1,0.3, 0));
-                 % qMatrix = jtraj(q1,q2,steps); % Obtaing the joint space trajectory
+                end
 
-             robot.model.animate(qMatrix(n, :));
+                if self.CheckCollision(robot.model, birdOnBranchPoints)
+                                            
+                   currentPos = robot.model.fkine(robot.model.getpos).t;
+                    q1 = robot.model.ikcon(transl(currentPos));
+                    q2 = robot.model.ikcon(transl(1,0.3, 0));
+                    qMatrix = jtraj(q1,q2,steps);
+                    n = 1;                   
 
 
+               end
 
-               self.CheckCollision(robot.model, birdOnBranchPoints);
                pause(0.01);
-               
-               pause(0.01);
-
-
-
-
-
 
 
                axis equal
@@ -69,66 +75,59 @@ classdef treeBotCollisionCheck < handle
             
         end
 
-        function CheckCollision(self,robot, xyzLimits)
+        function crash = CheckCollision(~,robot, xyzLimits)
 
-            currentPos = robot.fkine(robot.getpos).t; % Extract the position as a 3D vector
+            currentPos = robot.fkine(robot.getpos).t; % Extracting the position as a 3D vector            
 
             withinXlim = (currentPos(1) >= xyzLimits(1, 1)) && (currentPos(1) <= xyzLimits(1, 2));
             withinYlim = (currentPos(2) >= xyzLimits(2, 1)) && (currentPos(2) <= xyzLimits(2, 2));
             withinZlim = (currentPos(3) >= xyzLimits(3, 1)) && (currentPos(3) <= xyzLimits(3, 2));
              
-            withinLimits = withinXlim && withinYlim && withinZlim; % Determine if the position is within all limits
+            withinLimits = withinXlim && withinYlim && withinZlim; 
         
-            % Check for collision
             if withinLimits
+                crash = true;
                 disp("Crash");
-                %add way points that have a lower z value
-                % currentPos = robot.fkine(robot.getpos)
-                projectedPos = currentPos 
-                projectedPos(3,1) = currentPos(3,1) - 0.01;
-                projectedPos(1,1) = currentPos(1,1) -0.01;
-                
+                currentPos = robot.fkine(robot.getpos).t; 
+                projectedPos = currentPos - 0.03; 
+
                 q1 = robot.ikcon(transl(currentPos));
                 q2 = robot.ikcon(transl(projectedPos));
-                % 
+
                 steps = 30;
-                qMatrix = jtraj(q1,q2,steps); % Obtaing the joint space trajectory
-                
-                for n = 1:steps
-                   robot.animate(qMatrix(n, :));
-                  if self.emergencyStopPressed
-                       disp("Emergency stop button pressed!! Stopping Robot.");
-                       return;
-                   end
-                   pause(0.1) 
-
-                end
-
-            
-                % 
-
+                qMatrixCollision = jtraj(q1, q2, steps);        
                     
-            else
+                for o = 1:steps
+                    robot.animate(qMatrixCollision(o, :));
+                    pause(0.01);
+                end
+    
+             end
+
+
+            if ~withinLimits
                 disp("No Collision");
+                crash = false;
             end
                     
         end
 
         
         function detectES(self)
-            % Create a figure
-            f = figure('KeyPressFcn', @(src, event) self.detectedEM(src, event));
+
+            f = figure('KeyPressFcn', @(src, event) self.detectedEM(src, event)); % Creating emergency stop figure
             PlaceObject('emergencyStopButton.ply');
             
         end
         
         function detectedEM(self,~, event)
-            % Check if the pressed key is the space bar
-            if strcmp(event.Key, 'space')  
+            
+            if strcmp(event.Key, 'space')  % Checking if the pressed key is the space bar
                 self.emergencyStopPressed = true;
             end
         end
     end
 end
+
 
 
