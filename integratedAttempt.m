d2063c69842cba2b2d71c5f315ee76767b0e7df7
eleanorlow <160460243+eleanorlow@users.noreply.github.com>
2024-10-28 10:05:46 +1100

    editing

--- integratedAttempt.m
+++ integratedAttempt.m
@@ -1,17 +1,23 @@
 
 birdOnBranchPosition = [1.1,1.3,0.1;0,0,0];
 
-birdHousePrintingOffset = [0,0.2,0];
+% birdHousePrintingOffset = [0,0.2,0];
+% 
+% birdhousePos1 = [1,0.5,0];
+% birdhouseDest1 = [1.1,0.7,0.3];
+% 
+% birdhousePos2 = birdhousePos1 + birdHousePrintingOffset;
+% birdhouseDest2 = birdhouseDest1 + birdHousePrintingOffset;
             
 birdhousePos1 = [1,0.5,0];
 birdhouseDest1 = [1.1,0.7,0.3];
             
-birdhousePos2 = birdhousePos1 + birdHousePrintingOffset;
-birdhouseDest2 = birdhouseDest1 + birdHousePrintingOffset;
+
 
 %currentPos = robot.model.fkine(robot.model.getpos).t.';
 trajM = [birdhousePos1; birdhouseDest1; birdhousePos2; birdhouseDest2];
-collisionChecker = trying(trajM, birdOnBranchPosition);
+collisionChecker = treeBotIntegrated(trajM, birdOnBranchPosition, birdhousePos1, birdhouseDest1);
+hold on
 
 collisionChecker.runRobot()
 
