d1fa43c7d9fdf49f9814aca58b19429012db450a
Rhys <114918822+rhys-droid@users.noreply.github.com>
2024-10-28 11:27:33 +1100

    Add files via upload
    updated files

--- main.m
+++ main.m
@@ -2,12 +2,12 @@
 clc;
 clear;
 
-% Create the world and environment
 world = World();
 
 % Get the robot from the world
 robot_dobot = world.getRobot();
 robot_treebot = world.getRobot2();
+
 % 
 % robot_dobot.PlotAndColourRobot();
 % robot_treebot.PlotandColourRobot();
@@ -15,9 +15,33 @@ robot_treebot = world.getRobot2();
 robot_dobot.model.plot(world.qinit, 'workspace', world.workspace, 'scale', world.scale);
 robot_treebot.model.plot(world.qinit2, 'workspace', world.workspace, 'scale', world.scale);
 
-% Pass the robot to the printing function and start printing
-printer = printing(robot_dobot);
+num_birdhouses = size(printer.birdhouse_positions, 1);
 
-% After printing is done, pick up the prints
-pickup = PickUp(robot_treebot);
-pickup.startPickUp();
+spmd
+    if (spmdSend == 1)
+printer = printing(robot_dobot);   
+
+for i = 1:num_birdhouses
+   printer.printBirdhouse(i);
+   spmdSend(i, 2);
+   pause(1);
+end
+
+    elseif (spmdSend == 2)
+    pickup = PickUp(robot_treebot); 
+    for i = 1:num_birdhouses
+        birdhouse_num = spmdReceive(1);
+        if (i == 1)
+        target_position = [-0.2, 2, 1.2];
+
+        else
+             target_position = [0.2, 2, 1.2];
+        end
+    
+        start_pos = [printer.birdhouse_positions(birdhouse_num, :)]; 
+        picker.pickUpBirdhouse(start_pos, target_position);
+        pause(1);
+
+    end
+    end
+end
